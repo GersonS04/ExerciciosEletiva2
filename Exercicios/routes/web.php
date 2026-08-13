ed4020a7a227f80737b02c6382c734d50e001e83
Gerson <gersonscolari2@gmail.com>
2026-08-12 21:54:58 -0300

    Exercicio_3

--- Exercicios/routes/web.php
+++ Exercicios/routes/web.php
@@ -13,3 +13,6 @@ Route::post('/exer1resp', [ExercicioController::class, 'respostaExer1']);
 
 Route::get('/exer2', [ExercicioController::class, 'abrirFormExer2']);
 Route::post('/exer2resp', [ExercicioController::class, 'respostaExer2']);
+
+Route::get('/exer3', [ExercicioController::class, 'abrirFormExer3']);
+Route::post('/exer3resp', [ExercicioController::class, 'respostaExer3']);
